88d345bc4b9248b8d40215f2769e610244ea3daa
Nicholas Kibet <nickkorir08@gmail.com>
2024-07-11 11:43:29 +0300

    modifying elements in DOM

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+if (mainElement) {
+  mainElement.remove();
+}
+
+
+window.newHeader = document.createElement('h1');
+
+
+window.newHeader.id = 'victory';
+
+
+window.newHeader.textContent = 'Nick is the champion'; 
+
+
+document.body.appendChild(window.newHeader);
